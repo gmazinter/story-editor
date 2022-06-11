e0800b4711622f8ffdd62760e6483767ba024582
Guy Mazinter <gmazinter@gmail.com>
2022-06-11 08:48:22 +0000

    very basic router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+import { Router, Routes, Route } from "solid-app-router";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import Editor from "./pages/editor";
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<div class={styles.App}>
+							<header class={styles.header}>
+								<img src={logo} class={styles.logo} alt="logo" />
+								<p>
+									Edit <code>src/App.tsx</code> and save to reload.
+								</p>
+								<a
+									class={styles.link}
+									href="https://github.com/solidjs/solid"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Learn Solid
+								</a>
+							</header>
+						</div>
+					}
+				/>
+				<Route path="/editor" element={<Editor />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
---
+++ src/pages/editor/Editor.tsx
@@ -0,0 +1,5 @@
+import type { Component } from "solid-js";
+
+export const Editor: Component = () => {
+	return <div>hello editor world</div>;
+};
