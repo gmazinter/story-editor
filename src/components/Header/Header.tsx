cf83c73a4ab493b382b988b3a14748dce2438f43
Guy Mazinter <gmazinter@gmail.com>
2022-06-11 09:18:10 +0000

    added styled components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,10 @@
 import type { Component } from "solid-js";
+import { styled } from "solid-styled-components";
 
 export const Header: Component = () => {
-	return <div>i am the best header</div>;
+	return <HeaderContainer>i am the best editor app out there</HeaderContainer>;
 };
+
+const HeaderContainer = styled("div")`
+	background-color: red;
+`;
